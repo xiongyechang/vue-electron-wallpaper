b3703531160f44419ed10fa8dbb681e31bfdcec8
xiongyechang <2226791767@qq.com>
2021-12-12 21:05:03 +0800

    配合后端修改

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 import { version, name } from "../../package.json";
 
-export const baseURL = `http://127.0.0.1:3000/`;
+export const baseURL = `http://127.0.0.1:8888/`;
 // exports.baseURL = `http://47.94.162.34:8888/`;
 
 export const downloadURL = `https://cdn.xiongyechang.com/${name}@${version}.exe`;
--- vue.config.js
+++ vue.config.js
@@ -85,4 +85,15 @@ module.exports = {
       },
     },
   },
+  devServer: {
+    open: true,
+    host: '127.0.0.1',
+    port: 3000,
+    https: false,
+    hotOnly: false,
+    proxy: null, // 设置代理
+    // before: app => {
+      // console.log(app);
+    // }
+  },
 };
